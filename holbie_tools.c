b0d83ba0964f511dde3b2ac0e7807103a64a3563
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-12 15:17:35 -0500

    Feat(Task2): Code was update and prompt is updateable holbie_tools.c

--- holbie_tools.c
+++ holbie_tools.c
@@ -14,6 +14,19 @@ int _strstr(char *s1, char *s2)
 	return (0);
 }
 
+int _strncmp(char *s1, char *s2, int n)
+{
+	while(*s1 == *s2 && n != 0)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n == 0 && *s2 == 0)
+		return (1);
+	return (0);
+}
+
 
 /**
  * _strlen - returns the length of a string.
