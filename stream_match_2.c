a41482f0aa5f4ccda23beac83a902477c166f585
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-25 02:49:38 -0500

    Feat(Task 7 - 21): The Gates of Shell is complete stream_match_2.c

---
+++ stream_match_2.c
@@ -0,0 +1,94 @@
+#include "main.h"
+
+/**
+ * pipe_mode - Creates a pipe
+ * @go: Pointer to Structure General
+ * @file: Name of file or key
+ * @res: File Descriptor Result
+ *
+ * Return: Exit if it fails or field otherwise
+ */
+int pipe_mode(general *go, char *file, int res)
+{
+	int pipefd[2];
+
+	(void) file;
+	(void) res;
+
+	go->std_out = dup(STDOUT_FILENO);
+	if (pipe(pipefd) == -1)
+		exit(1);
+	go->fd = pipefd[0];
+	go->fd1 = pipefd[1];
+	if (dup2(go->fd1, STDOUT_FILENO) == -1)
+		exit(1);
+	close(go->fd1);
+
+	return (FIELD);
+}
+
+/**
+ * semicolon - Just assigns a field to receive another command
+ * @go: Pointer to Structure General
+ * @file: Name of file or key
+ * @res: File Descriptor Result
+ *
+ * Return: Field always
+ */
+int semicolon(general *go, char *file, int res)
+{
+	(void) go;
+	(void) file;
+	(void) res;
+	return (FIELD);
+}
+
+/**
+ * ampersand - In Process
+ * @go: Pointer to Structure General
+ * @file: Name of file or key
+ * @res: File Descriptor Result
+ *
+ * Return: Field always
+ */
+int ampersand(general *go, char *file, int res)
+{
+	(void) go;
+	(void) file;
+	(void) res;
+	return (FIELD);
+}
+
+/**
+ * double_pipe - In Process
+ * @go: Pointer to Structure General
+ * @file: Name of file or key
+ * @res: File Descriptor Result
+ *
+ * Return: Field always
+ */
+int double_pipe(general *go, char *file, int res)
+{
+	(void) go;
+	(void) file;
+	(void) res;
+	return (FIELD);
+}
+
+/**
+ * hash - Simply assigns a field and specify the end
+ * to ignore the following commands
+ * @go: Pointer to Structure General
+ * @file: Name of file or key
+ * @res: File Descriptor Result
+ *
+ * Return: Field always
+ */
+int hash(general *go, char *file, int res)
+{
+	(void) go;
+	(void) file;
+	(void) res;
+	go->end = 1;
+	return (FIELD);
+}
