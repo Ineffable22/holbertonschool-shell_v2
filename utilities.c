84e7b5622f34e9e832f490528d4ab47ac3632a6a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-25 02:49:40 -0500

    Feat(Task 7 - 21): The Gates of Shell is complete utilities.c

--- utilities.c
+++ utilities.c
@@ -1,30 +1,114 @@
 #include "main.h"
 
+/**
+ * printer - Prints the environment variables
+ * @env: Pointer to Singly linked list with the environment variables
+ *
+ * Return: Nothing
+ */
 void printer(envi *env)
 {
-	while(env)
+	while (env)
 	{
 		printf("%s=%s\n", env->key, env->value);
 		env = env->next;
 	}
 }
 
-void _free(envi *env)
+/**
+ * _free_env - Free the environment variables of the Singly linked Envi
+ * @env: Pointer to Singly linked list with the environment variables
+ * @PS1: Custom prompt
+ *
+ * Return: Nothing
+ */
+void _free_env(envi *env, char *PS1)
 {
 	if (env)
 	{
-		_free(env->next);
+		_free_env(env->next, PS1);
 		free(env->key);
 		free(env->value);
 		free(env);
 	}
+	else
+	{
+		if (PS1)
+			free(PS1);
+	}
 }
 
-void free_section(char **token, int res)
+/**
+ * _free_tkn - Free the tokens of the Singly linked list Tokens
+ * @tkn: Pointer to Singly linked list Tokens
+ *
+ * Return: Nothing
+ */
+void _free_tkn(tokens *tkn)
 {
-	while(token[res])
+	if (tkn)
 	{
-		free(token[res]);
-		res++;
+		_free_tkn(tkn->next);
+		free(tkn->token);
+		free(tkn);
+	}
+}
+
+/**
+ * _free_double - Free a double pointer of chars
+ * @token: Double pointer to free
+ *
+ * Return: Nothing
+ */
+void _free_double(char **token)
+{
+	int i = 0;
+
+	while (token[i])
+	{
+		free(token[i]);
+		i++;
+	}
+	free(token);
+}
+
+/**
+ * message_error - Prints a error message
+ * @go: Pointer to Structure General
+ * @val: Value to evaluate
+ * @tok: String to print
+ *
+ * Return: Nothing
+ */
+void message_error(general *go, char val, char *tok)
+{
+	if (val == 0)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("%s: %s: %s: %s\n", go->exe, go->token[0], go->msg, go->token[1]);
+		else
+			printf("%s: %d: %s: %s: %s\n", go->exe, go->n,
+			go->token[0], go->token[1], go->msg);
+	}
+	else if (val == 1)
+	{
+		go->msg = "No such file or directory";
+		if (isatty(STDIN_FILENO))
+			printf("%s: %s: %s\n", go->exe, tok, go->msg);
+		else
+			printf("%s: %d: %s: %s\n", go->exe, go->n, tok, go->msg);
+	}
+	else if (val == 2)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("%s: command not found\n", go->token[0]);
+		else
+		{
+			if (go->res == 2)
+				printf("%s: %d: '%s %s %s'\n", go->exe, go->n,
+				go->token[0], go->token[1], go->token[2]);
+			else
+				printf("%s: %d: %s: not found\n", go->exe, go->n, go->token[0]);
+		}
 	}
 }
