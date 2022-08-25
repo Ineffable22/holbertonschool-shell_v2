e2f7a414eac051cd419aac613a14756b6cd394bb
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-25 02:49:42 -0500

    Feat(Task 7 - 21): The Gates of Shell is complete _getline.h

---
+++ _getline.h
@@ -0,0 +1,26 @@
+#ifndef _GETLINE_H
+#define _GETLINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define READ_SIZE 4096
+
+/**
+ * struct Superbuffer - Struct that stores all the flags in the stream.
+ * This structure was invoked as static.
+ * @buff: Is a pointer that stores everything read by position.
+ * @data: Is a pointer with flags to create_stream function
+ * @next: Pointer to the next node in the struct Superbuffer
+ */
+typedef struct Superbuffer
+{
+	char buff[READ_SIZE];
+	int data[4];
+	struct Superbuffer *next;
+} supabuffa;
+
+char *_getline(const int fd);
+
+#endif
