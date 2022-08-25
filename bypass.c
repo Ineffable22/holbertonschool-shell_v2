63188ca6ee0d0c0bdf10200cf74a97f5136a5f33
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-25 03:00:24 -0500

    Feat(Change): strcpy to _strcpy bypass.c

--- bypass.c
+++ bypass.c
@@ -182,8 +182,8 @@ void add_history(general *go)
 	section = search_env("HOME", go->env);
 
 	file = malloc((_strlen(name) + _strlen(section->value) + 1) * sizeof(char));
-	strcpy(file, section->value);
-	strcpy(&file[_strlen(section->value)], name);
+	_strcpy(file, section->value);
+	_strcpy(&file[_strlen(section->value)], name);
 
 	fd = open(file, O_CREAT | O_APPEND | O_RDWR, 0666);
 	if (fd == -1)
